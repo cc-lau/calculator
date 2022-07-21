a800a63f4ab79da83b03a7371d73ad45ee29a6ba
Christian Lau <christianlau03@gmail.com>
2022-07-21 10:53:56 -0600

    Added working multiplication function

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ function subtract (num1, num2) {
 }
 
 //console.log(subtract(10, 5));
+
+//Multiplication Function
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(10, 5));
