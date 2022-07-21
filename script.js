1f7f64937496246230c5125fb9ce2c8647e609ef
Christian Lau <christianlau03@gmail.com>
2022-07-21 11:40:47 -0600

    Added function to allow user to input 2 numbers and an operator to complete a calculation

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
+//User input num1 & num2
+let n1 = prompt("Enter num 1: ")
+let operator = prompt("Enter operator: ")
+let n2 = prompt("Enter num 2: ")
+//Convert String input to Int
+let num1 = parseInt(n1);
+let num2 = parseInt(n2);
+
 //Addition Function
 function add (num1, num2) {
-    return num1 + num2;
+    addValue = (num1 + num2)
+    return addValue;
 }
-
-//console.log(subtract(10, 5));
 
 //Subtraction function
 function subtract (num1, num2) {
-    return num1 - num2;
+    subtractValue = (num1 - num2)
+    return subtractValue;
 }
-
-//console.log(subtract(10, 5));
 
 //Multiplication Function
 function multiply (num1, num2) {
-    return num1 * num2;
+    multiplyValue = (num1 * num2);
+    return multiplyValue;
 }
-
-//console.log(multiply(10, 5));
 
 //Division Function
 function divide (num1, num2) {
-    return num1 / num2;
+    divideValue = (num1 / num2);
+    return divideValue;
 }
 
-//console.log(divide(10, 5));
+//Operate function to check operator and call correct function
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            alert(add(num1, num2));
+            break;
+        case "-":
+            alert(subtract(num1, num2));
+            break;       
+        case "*":
+            alert(multiply(num1, num2));
+            break;
+        case "/":
+            alert(divide(num1, num2));
+            break;
+    }
+
+}
+
+operate(operator, num1, num2);
