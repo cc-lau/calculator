a8e3d709ba7b598f0a2930435d5b428820df4137
Christian Lau <christianlau03@gmail.com>
2022-07-21 10:51:34 -0600

    Added working subtraction function

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function add (num1, num2) {
     return num1 + num2;
 }
+
+//console.log(subtract(10, 5));
+
+//Subtraction function
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+//console.log(subtract(10, 5));
