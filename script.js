66695833c690c3f32ff58615d817a83343483e09
Christian Lau <christianlau03@gmail.com>
2022-07-25 12:14:28 -0600

    Add calculator functionality and display operation totals on screen

--- script.js
+++ script.js
@@ -2,23 +2,94 @@
 const input = document.querySelector('.screen')
 const numButton = document.querySelectorAll('.num')
 const opButton = document.querySelectorAll('.operator')
+const equalButton = document.querySelector('.equal')
 const clearButton = document.querySelector('.clear')
 const deleteButton = document.querySelector('.delete')
+let currentNum = '';
+let previousNum = '';
+let operator = '';
+
 
 //Display num on screen
 numButton.forEach(button => {
     button.addEventListener('click', (e) => {
-        const value = e.target.dataset.value
-        input.textContent += value
+        handleNumber(e.target.dataset.value);
     })
 })
 
+//Current number is equal to currentNum
+function handleNumber(number) {
+    if (currentNum.length <= 12) {
+    currentNum += number;
+    input.textContent = currentNum;
+    }
+}
+
+//
 opButton.forEach(button => {
     button.addEventListener('click', (e) => {
-        const operator = e.target.dataset.value
-        input.textContent += operator
+        handleOperator(e.target.dataset.value)
     })
 })
+
+function handleOperator (op) {
+    operator = op;
+    previousNum = currentNum;
+    currentNum = "";
+}
+
+equalButton.addEventListener('click', () => {
+    if(currentNum != "" && previousNum != "") {
+        operate(operator, currentNum, previousNum);
+    }
+});
+
+//Operate function to check operator and call correct function
+function operate(operator, currentNum, previousNum) {
+    currentNum = Number(currentNum);
+    previousNum = Number(previousNum);
+    switch (operator) {
+        case "+":
+            add(currentNum, previousNum);
+            break;
+        case "-":
+            subtract(currentNum, previousNum);
+            break;       
+        case "*":
+            multiply(currentNum, previousNum);
+            break;
+        case "/":
+            divide(currentNum, previousNum);
+            break;
+    }
+
+}
+
+/**Arithmetic Functions**/
+//Addition Function
+function add (currentNum, previousNum) {
+    previousNum = previousNum + currentNum;
+    input.textContent = previousNum;
+}
+
+//Subtraction function
+function subtract (currentNum, previousNum) {
+    previousNum = previousNum - currentNum;
+    input.textContent = previousNum;
+}
+
+//Multiplication Function
+function multiply (currentNum, previousNum) {
+    previousNum = previousNum * currentNum;
+    input.textContent = previousNum;
+}
+
+//Division Function
+function divide (currentNum, previousNum) {
+    previousNum = previousNum / currentNum;
+    input.textContent = previousNum;
+}
+
 
 /* CLEAR AND DELETE BUTTON ----- TO FIX
 clearButton.forEach(button => {
@@ -35,48 +106,3 @@ deleteButton.forEach(button => {
     })
 })
 */
-
-/**Arithmetic Functions**/
-//Addition Function
-function add (num1, num2) {
-    addValue = (num1 + num2)
-    input.textContent = addValue;
-}
-
-//Subtraction function
-function subtract (num1, num2) {
-    subtractValue = (num1 - num2)
-    input.textContent = subtractValue;
-}
-
-//Multiplication Function
-function multiply (num1, num2) {
-    multiplyValue = (num1 * num2);
-    input.textContent = multiplyValue;
-}
-
-//Division Function
-function divide (num1, num2) {
-    divideValue = (num1 / num2);
-    input.textContent = divideValue;
-}
-
-//Operate function to check operator and call correct function
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case "+":
-            alert(add(num1, num2));
-            break;
-        case "-":
-            alert(subtract(num1, num2));
-            break;       
-        case "*":
-            alert(multiply(num1, num2));
-            break;
-        case "/":
-            alert(divide(num1, num2));
-            break;
-    }
-
-}
-//operate(operator, num1, num2);
